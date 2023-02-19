44c6f28945a84f633b0a9e76fab042e895176949
potato <jaafariramadan@gmail.com>
2023-02-19 20:35:46 +0200

    added simple index page

--- app.js
+++ app.js
@@ -18,6 +18,12 @@ app.use(express.static(path.join(__dirname, 'public')));
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
 
+
+app.get('/', (req, res, next) => {
+    res.render('index');
+});
+
+
 app.listen(PORT, () => {
     console.log("Server is running on port " + PORT);
 });
