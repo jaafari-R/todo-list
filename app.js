767f89e2d57cbcb84f594ecd89a55cb5d2fc8e85
potato <jaafariramadan@gmail.com>
2023-02-19 20:22:44 +0200

    Initialized express js app and set up the middleware

---
+++ app.js
@@ -0,0 +1,23 @@
+const express = require('express');
+const path = require('path');
+const bodyParser = require('body-parser');
+
+const PORT = 14783;
+
+const app = express();
+
+// Body Parser Middleware
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({
+    extended: false
+}));
+app.use(express.static(path.join(__dirname, 'public')));
+
+
+// View Setup
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'ejs');
+
+app.listen(PORT, () => {
+    console.log("Server is running on port " + PORT);
+});
