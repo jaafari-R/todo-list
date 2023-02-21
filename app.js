0eb6075db9014fe5f7184dee302e5413a2888557
potato <jaafariramadan@gmail.com>
2023-02-21 18:10:12 +0200

    Added Tasks view and Tasks Insertion

--- app.js
+++ app.js
@@ -25,28 +25,53 @@ app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
 
 
-/* MongoDB client */
+/* ----- MongoDB client & db & collections ----- */
 const client = new MongoClient(MONGO_URL);
+client.connect();
+console.log('Connected to MongoDB successfully!');
+const db = client.db(DB_NAME);
+const Todos = db.collection('todos');
+
+
+/* ----- Queries ----- */
+async function getTodos()
+{
+    const todos = await Todos.find({}).toArray();
+    return todos;
+}
+
+async function addTodo(todo)
+{
+    await Todos.insertMany([todo], (err, result) => {
+        if(err) {
+            return console.log(err);
+        }
+        console.log("Task Added!");
+    });
+}
 
 
 /* ----- ROUTES ----- */
-app.get('/', (req, res, next) => {
-    res.render('index');
+app.get('/', async (req, res) => {
+    const todos = await getTodos();
+    // console.log(todos);
+    res.render('index', {
+        todos: todos
+    });
+});
+
+app.post('/todo/add', async (req, res) => {
+    const todo = {
+        task: req.body.task,
+        desc: req.body.desc
+    }
+
+    await addTodo(todo);
+    res.redirect('/');
 });
 
 
-async function main() {
-    /* Connect to MongoDB */
-    await client.connect();
-    console.log('Connected to MongoDB successfully!');
-    const db = client.db(DB_NAME);
-    const Todos = db.collection('todos');
-
-    /* ----- Server Start ----- */
-    app.listen(PORT, () => {
-        console.log("Server is running on port " + PORT);
-    });
-
-}
-
-main()
+/* ----- Server Start ----- */
+app.listen(PORT, () => {
+    console.log("Server is running on port " + PORT);
+});
