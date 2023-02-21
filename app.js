d41d0ecbf66569a5c9c2543e49d6a0c12c9c5568
potato <jaafariramadan@gmail.com>
2023-02-21 14:28:44 +0200

    Added MongoDB connection

--- app.js
+++ app.js
@@ -1,12 +1,18 @@
 const express = require('express');
 const path = require('path');
 const bodyParser = require('body-parser');
+const { MongoClient } = require('mongodb');
 
 const PORT = 14783;
 
+const MONGO_URL = 'mongodb://localhost:27017';
+const DB_NAME = 'todo-list-app';
+
+// App init
 const app = express();
 
-// Body Parser Middleware
+
+/* ----- Middleware ----- */
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({
     extended: false
@@ -14,16 +20,33 @@ app.use(bodyParser.urlencoded({
 app.use(express.static(path.join(__dirname, 'public')));
 
 
-// View Setup
+/* ----- View Setup ----- */
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
 
 
+/* MongoDB client */
+const client = new MongoClient(MONGO_URL);
+
+
+/* ----- ROUTES ----- */
 app.get('/', (req, res, next) => {
     res.render('index');
 });
 
 
-app.listen(PORT, () => {
-    console.log("Server is running on port " + PORT);
-});
+async function main() {
+    /* Connect to MongoDB */
+    await client.connect();
+    console.log('Connected to MongoDB successfully!');
+    const db = client.db(DB_NAME);
+    const Todos = db.collection('todos');
+
+    /* ----- Server Start ----- */
+    app.listen(PORT, () => {
+        console.log("Server is running on port " + PORT);
+    });
+
+}
+
+main()
